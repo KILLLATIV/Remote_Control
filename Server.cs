40cd3bfd1873462979c5d0d8be4d5b33b7502680
Vitalij Vlasov <vitalik.killativ@mail.ru>
2020-08-11 23:23:18 +0300

    First commit

---
+++ Server.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+// https://github.com/KILLLATIV/Remote_Control
+
+namespace Remote_Control 
+{
+    class Server
+    {
+        private const int Port = 4444;
+        private const string IP = "192.168.1.72";
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    TcpListener server = new TcpListener(IPAddress.Parse(IP), Port);
+                    StringBuilder request = new StringBuilder();
+                    server.Start();
+                    TcpClient client = server.AcceptTcpClient();
+                    NetworkStream stream = client.GetStream();
+
+                    while (true)
+                    {
+                        request.Clear();
+                        byte[] receiveData = new byte[256];
+
+                        do
+                        {
+                            int bytes = stream.Read(receiveData, 0, receiveData.Length);
+                            request.Append(Encoding.UTF8.GetString(receiveData, 0, bytes));
+                        }
+                        while (stream.DataAvailable);
+                        string response = ExecuteCommand(request.ToString());
+
+                        byte[] sendData = Encoding.UTF8.GetBytes(response);
+                        stream.Write(sendData, 0, sendData.Length);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        public static string ExecuteCommand(string command)
+        {
+            try
+            {
+                System.Diagnostics.ProcessStartInfo procStartInfo =
+                    new System.Diagnostics.ProcessStartInfo("cmd", "/c " + command);
+                procStartInfo.RedirectStandardOutput = true;
+                procStartInfo.UseShellExecute = false;
+                procStartInfo.CreateNoWindow = true;
+                System.Diagnostics.Process proc = new System.Diagnostics.Process();
+                proc.StartInfo = procStartInfo;
+                proc.Start();
+                string result = proc.StandardOutput.ReadToEnd();
+                return result;
+            }
+            catch (Exception objException)
+            {
+                return objException.Message;
+            }
+        }
+    }
+}
